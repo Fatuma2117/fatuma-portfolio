0e0d0a8b092f7ad396c7f51bd4d3edae5f9935bc
Fatuma Abdi <Abdi.fatuma776@gmail.com>
2022-07-20 17:47:21 -0500

    imported all components to app

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,12 +1,24 @@
 
 import './App.css';
 import React from "react";
+import Home from './Home';
+import About from './About';
+import Contact from './Contact';
+import Project from './Project';
+import { Route, Routes } from "react-router-dom";
+
 
 function App() {
   return (
-    <div>
-     <h1> This is App.js</h1>
-    </div>
+  <>
+   <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/project" element={<Project />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
+
+  </>
   );
 }
 
