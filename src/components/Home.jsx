d7a59bbee6bdf2831fb549b784a0e3045d03a24f
Fatuma Abdi <Abdi.fatuma776@gmail.com>
2022-07-20 21:50:05 -0500

    Landing Page component

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import { Navbar } from './Navbar/Navbar';
+import LandingPage from './LandingPage/LandingPage';
 
 const Home = () => {
   return (
     <div>
       <Navbar/>
+      <LandingPage/>
       
       </div>
   )
