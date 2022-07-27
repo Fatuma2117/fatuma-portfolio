be6cdba153cdf6e9660212975997b871c11bb67b
Fatuma Abdi <Abdi.fatuma776@gmail.com>
2022-07-27 17:45:51 -0500

    styling footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,5 +1,5 @@
 import "./Footer.css";
-import { FaHome, FaMailBulk, FaPhone } from "react-icons/fa";
+import { FaGithub, FaHome, FaLinkedin, FaMailBulk, FaPhone } from "react-icons/fa";
 
 import React from "react";
 
@@ -8,27 +8,47 @@ const Footer = () => {
     <div className="footer">
       <div className="footer-container">
         <div className="left">
-            <div className="location">
-                <FaHome size={20} style={{color: "#fff", marginRight: "2rem"}}/>
-            </div>
-            <div>
-                <p> Minneapolis, Minnesota</p>
-            </div>
-            <div className="phone">
-                <h4> <FaPhone size={20} style={{color: "#fff", marginRight: "2rem"}}/>
-                (612) 701-7161</h4>
-               
-
-            </div>
-            <div className="phone">
-                <h4> <FaMailBulk size={20} style={{color: "#fff", marginRight: "2rem"}}/>
-                Abdi.fatuma776@gmail.com</h4>
-               
-
+          <div className="location">
+            <FaHome size={20} style={{ color: "#fff", marginRight: "2rem" }} />
+          </div>
+          <div>
+            <p> Minneapolis, Minnesota</p>
+          </div>
+          <div className="phone">
+            <h4>
+              {" "}
+              <FaPhone
+                size={20}
+                style={{ color: "#fff", marginRight: "2rem" }}
+              />
+              (612) 701-7161
+            </h4>
+          </div>
+          <div className="phone">
+            <h4>
+              {" "}
+              <FaMailBulk
+                size={20}
+                style={{ color: "#fff", marginRight: "2rem" }}
+              />
+              Abdi.fatuma776@gmail.com
+            </h4>
+          </div>
+        </div>
+        <div className="right">
+            <h4> About the company</h4>
+            <p> New Projects and design challenges. </p>
+            <div className="social">
+            <FaLinkedin
+                size={30}
+                style={{ color: "#fff", marginRight: "1rem" }}
+              />
+                 <FaGithub
+                size={30}
+                style={{ color: "#fff", marginRight: "1rem" }}
+              />
             </div>
         </div>
-        <div className="right"></div>
-
       </div>
     </div>
   );
