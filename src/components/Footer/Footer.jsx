fdcba27236aef7773046ed9b4e72ac6773531ea0
Fatuma Abdi <Abdi.fatuma776@gmail.com>
2022-07-27 17:33:54 -0500

    added contact icons and text

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,5 +1,5 @@
 import "./Footer.css";
-import { FaHome } from "react-icons/fa";
+import { FaHome, FaMailBulk, FaPhone } from "react-icons/fa";
 
 import React from "react";
 
@@ -10,6 +10,21 @@ const Footer = () => {
         <div className="left">
             <div className="location">
                 <FaHome size={20} style={{color: "#fff", marginRight: "2rem"}}/>
+            </div>
+            <div>
+                <p> Minneapolis, Minnesota</p>
+            </div>
+            <div className="phone">
+                <h4> <FaPhone size={20} style={{color: "#fff", marginRight: "2rem"}}/>
+                (612) 701-7161</h4>
+               
+
+            </div>
+            <div className="phone">
+                <h4> <FaMailBulk size={20} style={{color: "#fff", marginRight: "2rem"}}/>
+                Abdi.fatuma776@gmail.com</h4>
+               
+
             </div>
         </div>
         <div className="right"></div>
