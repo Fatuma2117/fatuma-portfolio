61b2e9346ecfcd76afc9b91491e69eb5b4b4f11b
Fatuma Abdi <Abdi.fatuma776@gmail.com>
2022-07-27 16:17:39 -0500

    updated landingImg component

---
+++ src/components/Home.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { Navbar } from './Navbar/Navbar';
+import LandingImg from './LandingImg/LandingImg';
+
+const Home = () => {
+  return (
+    <div>
+      <Navbar/>
+      <LandingImg/>
+      
+      </div>
+  )
+}
+
+export default Home;
---
+++ src/components/LandingImg/LandingImg.jsx
@@ -0,0 +1,11 @@
+import "./LandingImg.css";
+
+import React from 'react'
+
+const LandingImg = () => {
+  return (
+    <div>LandingPage</div>
+  )
+}
+
+export default LandingImg;
