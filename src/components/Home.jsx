febfe443f87b8e8771081b2bab8ecfff8a825b88
Fatuma Abdi <Abdi.fatuma776@gmail.com>
2022-07-27 17:24:19 -0500

    created footer component

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import { Navbar } from './Navbar/Navbar';
 import LandingImg from './LandingImg/LandingImg';
+import Footer from './Footer/Footer'
 
 const Home = () => {
   return (
     <div>
       <Navbar/>
       <LandingImg/>
+      <Footer/>
       
       </div>
   )
