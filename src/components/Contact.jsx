e57761975250e5741eb670e23913f6529c54460f
Fatuma Abdi <Abdi.fatuma776@gmail.com>
2022-07-28 12:16:18 -0500

    created WorkCard component

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import { Navbar } from './Navbar/Navbar';
 import Footer from './Footer/Footer'
+import MainImg from './MainImg/MainImg'
 
 const Contact = () => {
   return (
     <div>
       <Navbar/>
+      <MainImg heading="Contact" text="Let's have a chat!"/>
       <Footer/>
      </div>
   )
