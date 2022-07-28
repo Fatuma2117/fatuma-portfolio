19b800d0b7fd33df06f48a86b441a975e9486467
Fatuma Abdi <Abdi.fatuma776@gmail.com>
2022-07-28 13:46:23 -0500

    project data array

--- src/components/ProjectData.jsx
+++ src/components/ProjectData.jsx
@@ -1,10 +1,19 @@
-
-import BBCover from "../images/BBCover.png"
-import LRTDash from "../images/LRTDash.png"
-import BBCover from "../images/BBCover.png"
+import BBCover from "../images/BBCover.png";
+import LRTDash from "../images/LRTDash.png";
 
 const ProjectCardData = [
-    {
-        imgsrc: BBCover;
-    }
-]
+  {
+    imgsrc: BBCover,
+    title: "Book Buddy Tracker",
+    text: "Book Buddy is a tool for kids to add current book, track completed books, rate overall experience and accrue reading points.",
+    view: "https://young-river-71141.herokuapp.com/",
+    source:"https://github.com/Fatuma2117/Book-Buddy"
+  },
+  {
+    imgsrc: LRTDash,
+    title: "Living Room Tutors",
+    text: "LRT creates matches between volunteer student mentors and students who need support.",
+    view: "https://www.livingroomtutors.org/",
+    source:"https://github.com/LRTDevs/LivingRoomTutors"
+  }
+];
