559e56eb40c13455ba75df1c417ed71b7ace4352
Fatuma Abdi <Abdi.fatuma776@gmail.com>
2022-07-28 12:50:00 -0500

    added project img and two button links

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -2,12 +2,14 @@ import React from 'react'
 import { Navbar } from './Navbar/Navbar';
 import LandingImg from './LandingImg/LandingImg';
 import Footer from './Footer/Footer'
+import WorkCard from './WorkCard/WorkCard';
 
 const Home = () => {
   return (
     <div>
       <Navbar/>
       <LandingImg/>
+      <WorkCard/>
       <Footer/>
       </div>
   )
--- src/components/WorkCard/WorkCard.jsx
+++ src/components/WorkCard/WorkCard.jsx
@@ -1,10 +1,28 @@
-import  "../WorkCard.css"
-
+import  "../WorkCard/WorkCard.css"
+import BBCover from "../images/BBCover.png"
 import React from 'react'
+import { Link, NavLink } from "react-router-dom"
+import { useHistory } from "react-router-dom";
 
 const WorkCard = () => {
+    // const history = useHistory();
   return (
-    <div>WorkCard</div>
+    <div className="work-container">
+        <h1 className="project-heading"> Projects</h1>
+        <div className="project-container">
+            <div className="project-card">
+                <img src={BBCover} alt="image"/>
+                <h2 className="title">Book Buddy Tracker</h2>
+                <div className="details">
+                    <p> This is BB details</p>
+                    <div className="pro-btn">
+                    <a className="btn" href="https://young-river-71141.herokuapp.com/" >View</a>
+                    <a className="btn" href="https://github.com/Fatuma2117/Book-Buddy" >Source</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
   )
 }
 
