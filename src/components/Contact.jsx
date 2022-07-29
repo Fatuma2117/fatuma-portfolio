7865c56ebe029a7fd8ddfb02b42d7f2f9329de17
Fatuma Abdi <Abdi.fatuma776@gmail.com>
2022-07-29 17:39:25 -0500

    added form inputs and labels

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -2,12 +2,14 @@ import React from 'react'
 import { Navbar } from './Navbar/Navbar';
 import Footer from './Footer/Footer'
 import MainImg from './MainImg/MainImg'
+import { Form } from './Form';
 
 const Contact = () => {
   return (
     <div>
       <Navbar/>
       <MainImg heading="Contact" text="Let's have a chat!"/>
+      <Form/>
       <Footer/>
      </div>
   )
--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -8,6 +8,13 @@ export const Form = () => {
         <form>
             <label> Your Name</label>
             <input type="text"></input>
+            <label>Email</label>
+            <input type="email"></input>
+            <label>Subject</label>
+            <input tpe="text"></input>
+            <label>Message</label>
+            <textarea rows={6} placeholder=" Type Your Message Here"/>
+            <button className="btn">Submit</button>
         </form>
         </div>
   )
