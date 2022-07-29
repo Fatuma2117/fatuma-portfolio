b95e430fadb388ce0f6f26e61f95a09d33970b28
Fatuma Abdi <Abdi.fatuma776@gmail.com>
2022-07-29 15:49:09 -0500

    styled cards

--- src/components/ProjectData.jsx
+++ src/components/ProjectData.jsx
@@ -16,20 +16,7 @@ const ProjectCardData = [
     view: "https://www.livingroomtutors.org/",
     source:"https://github.com/LRTDevs/LivingRoomTutors"
   },
-    {
-    imgsrc: BBCover,
-    title: "Book Buddy Tracker",
-    text: "Book Buddy is a tool for kids to add current book, track completed books, rate overall experience and accrue reading points.",
-    view: "https://young-river-71141.herokuapp.com/",
-    source:"https://github.com/Fatuma2117/Book-Buddy"
-  },
-  {
-    imgsrc: LRTDash,
-    title: "Living Room Tutors",
-    text: "LRT creates matches between volunteer student mentors and students who need support.",
-    view: "https://www.livingroomtutors.org/",
-    source:"https://github.com/LRTDevs/LivingRoomTutors"
-  }
+  
 ];
 
 export default ProjectCardData;
